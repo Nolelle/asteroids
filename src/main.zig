998a0e0172462a199e998c8718595e80cc76025a
Nolelle <eyu1@ualberta.ca>
2024-07-25 00:02:49 -0600

    add init constructor for ship struct and add update function

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,30 @@
-// raylib-zig (c) Nikolas Wipper 2023
-
 const rl = @import("raylib");
 
 pub const Ship = struct {
     position: rl.Vector2,
     velocity: rl.Vector2,
     scale: f32,
+    rotation: f32,
     color: rl.Color,
     vertices: [3]rl.Vector2,
+    acceleration: f32,
+    rotation_speed: f32,
+
+    pub fn init() Ship {
+        return .{
+            .position = rl.Vector2.init(300.0, 200.0),
+            .velocity = rl.Vector2.init(0.0, 0.0),
+            .scale = 1.0,
+            .color = rl.Color.black,
+            .vertices = [3]rl.Vector2{
+                rl.Vector2{ .x = 400.0, .y = 200.0 },
+                rl.Vector2{ .x = 385.0, .y = 225.0 },
+                rl.Vector2{ .x = 415.0, .y = 225.0 },
+            },
+        };
+    }
+    // ToDO
+    pub fn update() void {}
 };
 
 pub const Asteroid = struct {
@@ -26,22 +43,9 @@ pub fn main() anyerror!void {
     defer rl.closeWindow();
     rl.setTargetFPS(60);
 
-    const ship = Ship{
-        .position = rl.Vector2.init(300.0, 200.0),
-        .velocity = rl.Vector2.init(0.0, 0.0),
-        .scale = 1.0,
-        .color = rl.Color.black,
-        .vertices = [3]rl.Vector2{
-            rl.Vector2{ .x = 400.0, .y = 200.0 },
-            rl.Vector2{ .x = 385.0, .y = 225.0 },
-            rl.Vector2{ .x = 415.0, .y = 225.0 },
-        },
-    };
+    var ship = Ship{};
 
-    while (!rl.windowShouldClose()) { // Detect window close button or ESC key
-        // Variables here
-
-        // Draw
+    while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
         rl.clearBackground(rl.Color.white);
